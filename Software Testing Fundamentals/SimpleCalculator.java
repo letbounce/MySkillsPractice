4c4c16a051394eff08b9b7e73243074f2e67405c
Oleg Koksiuk <olegkoksiuk@gmail.com>
2025-07-04 13:29:35 +0300

    added simple calculator.java

---
+++ Software Testing Fundamentals/SimpleCalculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number A: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter number B: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = a + b;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = a - b;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = a * b;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                result = a / b;
+                System.out.println("Result: " + result);
+                break;
+            default:
+                System.out.println("Unknown operator");
+        }
+
+        scanner.close();
+    }
+}
